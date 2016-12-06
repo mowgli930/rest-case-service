221a7703b58f59527bb36612b8111282340a8e91
leo MBP <leo.moralesvega@gmail.com>
2016-12-06 13:44:51 +0100

    Initialized project

---
+++ src/main/java/se/plushogskolan/restcaseservice/RestCaseServiceApplication.java
@@ -0,0 +1,12 @@
+package se.plushogskolan.restcaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestCaseServiceApplication.class, args);
+	}
+}
