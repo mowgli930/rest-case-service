aa331485fdf95735a675172548bd1be6b3a12bc2
leo <leo.moralesvega@gmail.com>
2016-12-08 04:05:52 +0100

    Registered WorkItemResource in JerseyConfig class

--- src/main/java/se/plushogskolan/restcaseservice/config/JerseyConfig.java
+++ src/main/java/se/plushogskolan/restcaseservice/config/JerseyConfig.java
@@ -4,12 +4,14 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
 import se.plushogskolan.restcaseservice.resource.UserResource;
+import se.plushogskolan.restcaseservice.resource.WorkItemResource;
 
 @Component
 public class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
 		register(UserResource.class);
+		register(WorkItemResource.class);
 	}
 
 }
