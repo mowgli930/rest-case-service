5c3468bdfd1b2f31390f12b2fe99e535e7c0d941
leo <leo.moralesvega@gmail.com>
2016-12-08 04:08:16 +0100

    Fixed unimportant typo in comments

--- src/main/java/se/plushogskolan/restcaseservice/resource/WorkItemResource.java
+++ src/main/java/se/plushogskolan/restcaseservice/resource/WorkItemResource.java
@@ -66,7 +66,7 @@ public final class WorkItemResource {
 
 	@GET
 	public Response searchWorkItemDescription(@QueryParam("description") String description) {
-		//TODO implmement
+		//TODO implement
 		return null;
 	}
 	@GET
