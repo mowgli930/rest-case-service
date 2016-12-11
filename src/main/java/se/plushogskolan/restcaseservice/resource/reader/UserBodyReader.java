23a97b340a63a1f461dbf5e2c48784e07fde3675
Joakim Landström <joakim.landstrom@outlook.com>
2016-12-11 20:08:11 +0100

    fox

--- src/main/java/se/plushogskolan/restcaseservice/resource/reader/UserBodyReader.java
+++ src/main/java/se/plushogskolan/restcaseservice/resource/reader/UserBodyReader.java
@@ -1,7 +1,30 @@
 package se.plushogskolan.restcaseservice.resource.reader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 
+import se.plushogskolan.casemanagement.model.User;
+
 public class UserBodyReader implements MessageBodyReader<User> {
+
+	@Override
+	public boolean isReadable(Class<?> arg0, Type arg1, Annotation[] arg2, MediaType arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User readFrom(Class<User> arg0, Type arg1, Annotation[] arg2, MediaType arg3,
+			MultivaluedMap<String, String> arg4, InputStream arg5) throws IOException, WebApplicationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
