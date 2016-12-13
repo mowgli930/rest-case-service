46c8614b64cbafcae10591b5123ed08caff6648f
Joakim Landström <joakim.landstrom@outlook.com>
2016-12-13 16:48:24 +0100

    register issueresource

--- src/main/java/se/plushogskolan/restcaseservice/config/JerseyConfig.java
+++ src/main/java/se/plushogskolan/restcaseservice/config/JerseyConfig.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import se.plushogskolan.restcaseservice.exception.BadRequestExceptionMapper;
 import se.plushogskolan.restcaseservice.exception.ConflictExceptionMapper;
 import se.plushogskolan.restcaseservice.exception.NotFoundExceptionMapper;
+import se.plushogskolan.restcaseservice.resource.IssueResource;
 import se.plushogskolan.restcaseservice.resource.TeamResource;
 import se.plushogskolan.restcaseservice.resource.UserResource;
 import se.plushogskolan.restcaseservice.resource.WorkItemResource;
@@ -17,10 +18,10 @@ public class JerseyConfig extends ResourceConfig {
 		register(UserResource.class);
 		register(WorkItemResource.class);
 		register(TeamResource.class);
+		register(IssueResource.class);
 		register(ConflictExceptionMapper.class);
 		register(NotFoundExceptionMapper.class);
 		register(BadRequestExceptionMapper.class);
-//		register(IssueResource.class);
 //		register(DTOIssueService.class);
 	}
 
